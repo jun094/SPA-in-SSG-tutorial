636f64833eb34185ec5d4d2d0a3c783b6addfdda
dev-jun <42564107+jun094@users.noreply.github.com>
2022-02-02 16:46:27 +0900

    feat(EmotionJS): style 지정 방법
    - @emotion/react의 Global 함수를 통한 스타일 지정
    - @emotion/react의 css 함수를 통한 스타일 지정
    - @emotion/styled의 styled함수를 통한 스타일 지정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import { Link } from 'gatsby'
 
-import Text from 'components/Text'
-
 const IndexPage: FunctionComponent = function () {
   return (
-    <>
-      <Text text="Home" />
-      <Link to="/info">To Info</Link>
-    </>
+    <div>
+      <Link to="/info/">To Info</Link>
+    </div>
   )
 }
 
