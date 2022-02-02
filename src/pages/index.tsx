30959f15d3e9ea80c010e1fb0bc05745b034a1ec
dev-jun <42564107+jun094@users.noreply.github.com>
2022-02-02 18:06:09 +0900

    feat(Main): Category, List, Item 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 
 import GlobalStyle from 'components/Common/GlobalStyle'
-import Introduction from 'components/Main/Introduction'
 import Footer from 'components/Common/Footer'
+
+import Introduction from 'components/Main/Introduction'
+import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
@@ -16,6 +25,8 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
       <Footer />
     </Container>
   )
