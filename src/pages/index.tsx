3bbcf00a5cdc6fdabd92e1bdf815342572847b73
dev-jun <42564107+jun094@users.noreply.github.com>
2022-02-02 15:29:33 +0900

    feat(GraphQL): tutorial GraphQL
    - http://localhost:8000/___graphql 에 접근하면, 자체 ide 확인가능
    - 메타데이터 요청 가능

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Text'
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <>
+      <Text text="Home" />
+      <a href="/info/">To Info</a>
+    </>
+  )
+}
+
+export default IndexPage
