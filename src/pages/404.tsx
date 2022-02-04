f51d088d4c41291d34e01084e2615e2ed24df321
dev-jun <42564107+jun094@users.noreply.github.com>
2022-02-04 16:27:22 +0900

    feat(404): 404페이지 생성
    - pages 폴더 안에 404.tsx 생성하면 됨

---
+++ src/pages/404.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+import GlobalStyle from 'components/Common/GlobalStyle'
+
+const NotFoundPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const NotFoundText = styled.div`
+  font-size: 100px;
+  font-weight: 800;
+
+  @media (max-width: 768px) {
+    font-size: 60px;
+  }
+`
+
+const GoToMainButton = styled(Link)`
+  margin-top: 30px;
+  font-size: 20px;
+  font-weight: 400;
+
+  &:hover {
+    font-weight: 600;
+  }
+`
+
+const NotFoundPage: FunctionComponent = function () {
+  return (
+    <NotFoundPageWrapper>
+      <GlobalStyle />
+      <NotFoundText>404 Error :(</NotFoundText>
+      <GoToMainButton to="/">{'>'} GO TO MAIN</GoToMainButton>
+    </NotFoundPageWrapper>
+  )
+}
+
+export default NotFoundPage
