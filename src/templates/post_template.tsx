8e63da6542138f6b07bc0bc54866b575739d30f6
holymoly.jun <42564107+jun094@users.noreply.github.com>
2022-02-04 16:13:46 +0900

    [CH3] markdown 데이터를 활용한 동적페이지 생성 (#2)
    * feat(DynamicPage): slug 생성
    
    * feat(DynamicPage): dynamic page api 생성
      - gatsby createPages api 사용
    
    * feat(DynamicPage): markdown데이터로 동적 페이지 생성
    
    * feat(DynamicPage): markdonw 스타일링

---
+++ src/templates/post_template.tsx
@@ -0,0 +1,74 @@
+import React, { FunctionComponent } from 'react'
+import { graphql } from 'gatsby'
+
+import { PostPageItemType } from 'types/PostItem.types'
+
+import PageWrapper from 'components/Common/PageWrapper'
+import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
+
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]
+    }
+  }
+}
+
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary, // 나중에 사용할 예정입니다!
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+        },
+      },
+    },
+  } = edges[0]
+
+  return (
+    <PageWrapper>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html} />
+    </PageWrapper>
+  )
+}
+
+export default PostTemplate
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
